22906f5ddba1f310b432ab84ed06f81fb1e475ca
Mikhail Tsyvis <m.tsyvis@paralect.com>
2024-08-26 14:58:51 +0800

    add entity configurations

--- Bukify.Infrastructure/DependencyInjection.cs
+++ Bukify.Infrastructure/DependencyInjection.cs
@@ -2,16 +2,26 @@
 using Bookify.Application.Abstractions.Email;
 using Bukify.Infrastructure.Clock;
 using Bukify.Infrastructure.Email;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bukify.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddTransient<IDateTimeProvider, DateTimeProvider>();
         services.AddTransient<IEmailService, EmailService>();
+
+        var connectionString = configuration.GetConnectionString("Database")
+            ?? throw new InvalidOperationException("Database connection string is missing");
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
+        });
 
 
         return services;
